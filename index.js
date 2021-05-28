9e8a6da4db0cf60d7fa589ddeee0304c24e9a42f
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-28 14:05:33 -0400

    commit tree 52b62354293086c7e89136597c81f31ce592344e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()) + 1;
+
+
+let num3 = 64;
+let num4 = 12;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,20);
